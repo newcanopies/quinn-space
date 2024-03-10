519ae824fc1561256879e32f616b92d451e33545
Marc Blanchet <marc.blanchet@viagenie.ca>
2024-03-10 00:38:48 -0500

    mtu_discovery_config.interval set to 1000000s instead of Duration::MAX

--- quinn/examples/client.rs
+++ quinn/examples/client.rs
@@ -146,7 +146,7 @@ async fn run(options: Opt) -> Result<()> {
          // disable mtu discovery
          let mut mtu_discovery_config = MtuDiscoveryConfig::default();
          mtu_discovery_config.upper_bound(1200);  //should be INITIAL_MTU
-         mtu_discovery_config.interval(Duration::MAX);
+         mtu_discovery_config.interval(Duration::new(100000));
          transport_config.mtu_discovery_config(Some(mtu_discovery_config));
 
      }
