3016eb300635e2fd4dd53b7639e6f4ac3104e3e5
Marc Blanchet <marc.blanchet@viagenie.ca>
2024-03-09 21:10:47 -0500

    set mtu_discovery_config interval to infinite...

--- quinn/examples/client.rs
+++ quinn/examples/client.rs
@@ -146,6 +146,7 @@ async fn run(options: Opt) -> Result<()> {
          // disable mtu discovery
          let mut mtu_discovery_config = MtuDiscoveryConfig::default();
          mtu_discovery_config.upper_bound(1200);  //should be INITIAL_MTU
+         mtu_discovery_config.interval(Duration::MAX);
          transport_config.mtu_discovery_config(Some(mtu_discovery_config));
 
      }
